85211faf38fe4e61955006238245cb82460b485b
Josué Tenorio <tndjosue69@gmail.com>
2025-09-15 17:15:27 -0600

    New files added for shopping cart page tests

---
+++ Web/test_data/products.py
@@ -0,0 +1,11 @@
+# Web/test_data/products.py
+
+ART_HISTORY = "Art History"
+DENIM_JEANS  = "Denim Jeans"
+
+PRODUCTS = {
+    ART_HISTORY: {"id": 36, "unit_price": 39.99},
+    DENIM_JEANS: {"id": 2,  "unit_price": 79.99},
+}
+
+TAX_RATE = 0.08
