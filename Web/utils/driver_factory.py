ceab81a3ce62b4ff997396fa219ae2b320ae0ce9
Gonzalez Aguilar Santiago <zant.jobs@gmail.com>
2025-09-30 12:47:43 -0300

    Update driver_factory.py

--- Web/utils/driver_factory.py
+++ Web/utils/driver_factory.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import os, tempfile
+import shutil, tempfile
 
 
 #Crea el driver mostrando interfaz usuario
@@ -17,17 +17,10 @@ def create_driver(headless: bool = True):
 
     user_data_dir = tempfile.mkdtemp()
     options.add_argument(f"--user-data-dir={user_data_dir}")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--remote-debugging-port=9222")  # important for headless in CI
-    options.add_argument("--disable-dev-tools")
-    options.add_argument("--no-first-run")
-    options.add_argument("--no-default-browser-check")
 
-    chrome_path = os.getenv("CHROME_PATH")
-    if chrome_path:
-        options.binary_location = chrome_path
+    chrome_bin = shutil.which("google-chrome")
+    if chrome_bin:
+        options.binary_location = chrome_bin
 
 #instanciar web driver
     driver = webdriver.Chrome(
