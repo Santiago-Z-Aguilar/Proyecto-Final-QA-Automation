65fa6a053156ac285289cf7dcfff0c0e622793ea
Gonzalez Aguilar Santiago <zant.jobs@gmail.com>
2025-10-07 01:39:45 -0300

    added all web tests

--- Web/tests/e2e/conftest.py
+++ Web/tests/e2e/conftest.py
@@ -24,6 +24,7 @@ def driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--remote-allow-origins=*")
+    chrome_options.add_argument("--headless=new")
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.set_page_load_timeout(25)
